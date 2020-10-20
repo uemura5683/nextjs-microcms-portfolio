6c4420bc826f111a8c13af7f745b1d6daf9550c7
uemuragame5683 <nobuyoshi5683@gmail.com>
2020-10-21 01:32:27 +0900

    scssをコミット

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,22 @@
-import '../styles/global.css'
+import '../styles/foundation/reset.scss'
+import '../styles/foundation/base.scss'
+import '../styles/foundation/minin.scss'
+import '../styles/layout/l-header.scss'
+import '../styles/layout/l-drawer.scss'
+import '../styles/layout/l-main.scss'
+import '../styles/layout/l-footer.scss'
+import '../styles/object/component/c-button.scss'
+import '../styles/object/component/c-form.scss'
+import '../styles/object/component/c-grid.scss'
+import '../styles/object/component/c-title.scss'
+import '../styles/object/project/p-card.scss'
+import '../styles/object/project/p-link.scss'
+import '../styles/object/project/p-profile.scss'
+import '../styles/object/project/p-skill.scss'
+import '../styles/object/utility/u-color.scss'
+import '../styles/object/utility/u-margin.scss'
+import '../styles/object/utility/u-padding.scss'
+import '../styles/object/utility/u-utility.scss'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
