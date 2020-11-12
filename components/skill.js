ec5b0d5ef545f51a5b6cce25a1a7aa5695b418b7
uemuragame5683 <nobuyoshi5683@gmail.com>
2020-11-12 23:57:18 +0900

    ポートフォリオサイトをコミット

--- components/skill.js
+++ components/skill.js
@@ -1,53 +1,69 @@
 export default function Skill() {
   return (
       <ul class="skill__list">
-          <li class="skill__zoom css">
-            <img src="/images/skill/skill_css.png"></img>
-          </li>
-          <li class="skill__zoom go">
-            <img src="/images/skill/skill_go.png"></img>
-          </li>
-          <li class="skill__zoom gulp">
-            <img src="/images/skill/skill_gulp.png"></img>
-          </li>
           <li class="skill__zoom html">
             <img src="/images/skill/skill_html.png"></img>
-          </li>
-          <li class="skill__zoom jq">
-            <img src="/images/skill/skill_jq.png"></img>
-          </li>
-          <li class="skill__zoom js">
-            <img src="/images/skill/skill_js.png"></img>
-          </li>
-          <li class="skill__zoom mcms">
-            <img src="/images/skill/skill_mcms.png"></img>
-          </li>
-          <li class="skill__zoom mt">
-            <img src="/images/skill/skill_mt.png"></img>
-          </li>
-          <li class="skill__zoom next">
-            <img src="/images/skill/skill_next.png"></img>
-          </li>
-          <li class="skill__zoom node">
-            <img src="/images/skill/skill_node.png"></img>
-          </li>
-          <li class="skill__zoom nuxt">
-            <img src="/images/skill/skill_nuxt.png"></img>
-          </li>
-          <li class="skill__zoom php">
-            <img src="/images/skill/skill_php.png"></img>
-          </li>
-          <li class="skill__zoom pug">
-            <img src="/images/skill/skill_pug.png"></img>
-          </li>
-          <li class="skill__zoom scss">
-            <img src="/images/skill/skill_scss.png"></img>
-          </li>
-          <li class="skill__zoom vue">
-            <img src="/images/skill/skill_vue.png"></img>
+            <div class="gage"></div>
           </li>
           <li class="skill__zoom wp">
             <img src="/images/skill/skill_wp.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom css">
+            <img src="/images/skill/skill_css.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom mt">
+            <img src="/images/skill/skill_mt.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom js">
+            <img src="/images/skill/skill_js.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom mcms">
+            <img src="/images/skill/skill_mcms.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom jq">
+            <img src="/images/skill/skill_jq.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom vue">
+            <img src="/images/skill/skill_vue.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom php">
+            <img src="/images/skill/skill_php.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom nuxt">
+            <img src="/images/skill/skill_nuxt.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom scss">
+            <img src="/images/skill/skill_scss.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom next">
+            <img src="/images/skill/skill_next.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom pug">
+            <img src="/images/skill/skill_pug.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom node">
+            <img src="/images/skill/skill_node.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom go">
+            <img src="/images/skill/skill_go.png"></img>
+            <div class="gage"></div>
+          </li>
+          <li class="skill__zoom gulp">
+            <img src="/images/skill/skill_gulp.png"></img>
+            <div class="gage"></div>
           </li>
       </ul>
   )
