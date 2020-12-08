cd26875c6c0e97f295a057a027e1f552c155cb2c
uemuragame5683 <nobuyoshi5683@gmail.com>
2020-12-09 01:06:42 +0900

    お問い合わせをコミット

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,11 +1,43 @@
-export default function Contact() {
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+
+const Contacts = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const data = {
+      email: email,
+      name: name,
+      body: body
+    };
+
+    axios({
+      method: "post",
+      url: "https://your.microcms.io/api/v1/contacts",
+      data: data,
+      headers: {
+        "Content-Type": "application/json",
+        "X-WRITE-API-KEY": process.env.x_api_key
+      }
+    })
+      .then(() => {
+        router.push("/success");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   return (
     <div className="c-contact-form__inner">
       <p className="c-contact-form__txt">どんな些細でもいいですので気軽にお問い合わせください！<br></br><a href="https://twitter.com/uemuragame5683" target="_blank">Twitter</a>でも受け付けております。</p>
-      <form
-        action="https://api.staticforms.xyz/submit"
-        method="post"
-        onSubmit="">
+      <form>
         <div className="c-contact-form__content">
           <label>あなたの名前</label>
             <input
@@ -13,7 +45,7 @@ export default function Contact() {
               placeholder="名前を入力してください"
               id="form_name"
               name="name"
-              onChange=""
+              onChange={e => setEmail(e.target.value)}
               required
             />
         </div>
@@ -24,7 +56,7 @@ export default function Contact() {
               placeholder="メールアドレスを入力してください"
               id="form_email"
               name="mail_address"
-              onChange=""
+              onChange={e => setName(e.target.value)}
               required
             />
         </div>
@@ -35,7 +67,7 @@ export default function Contact() {
               placeholder="タイトルを入力してください"
               id="form_title"
               name="title"
-              onChange=""
+              onChange={e => setTitle(e.target.value)}
               required
             />
         </div>
@@ -46,7 +78,7 @@ export default function Contact() {
               placeholder="本文を入力してください"
               type="text"
               name="body"
-              onChange=""
+              onChange={e => setBody(e.target.value)}
               required
             />
           </div>
@@ -57,4 +89,6 @@ export default function Contact() {
       </form>
     </div>
   )
-}
+};
+
+export default Contacts;
