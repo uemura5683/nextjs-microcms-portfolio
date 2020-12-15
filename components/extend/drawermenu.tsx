97c6e59114793b6aeabc99658e62f35c13996af6
uemuragame5683 <nobuyoshi5683@gmail.com>
2020-12-15 22:07:58 +0900

    ドロワーメニューをコミット

--- components/extend/drawermenu.tsx
+++ components/extend/drawermenu.tsx
@@ -1,20 +1,22 @@
 
+import React from 'react';
 import Navi from '../../components/nav'
 
-const openDrawer = () => {
-  
-}
-
 const DrawerMenu = () => {
-  return (      
+
+  const [open, setOpen] = React.useState(true)
+  // open という名前の state 変数を宣言、初期値 true をセット
+  const toggle = () => setOpen(!open)
+
+  return (
     <>  
-      <button onClick={openDrawer} className="header--menu__btn">
+      <button onClick={toggle} className={open ? 'header--menu__btn isOpen' : 'header--menu__btn isClose'}>
         <span></span>
         <span></span>
         <span></span>
       </button>
-      <div className="header--menu__content">
-      <Navi></Navi>
+      <div className={open ? 'header--menu__content isOpen' : 'header--menu__content isClose'}>
+        <Navi></Navi>
       </div>
     </>
   )
