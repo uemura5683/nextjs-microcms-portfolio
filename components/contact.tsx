82eccba270c768ffc2b625af8bda820857162aef
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-01-11 18:22:31 +0900

    お問い合わせを編集

--- components/contact.tsx
+++ components/contact.tsx
@@ -7,24 +7,60 @@ Modal.setAppElement("#p-contact");
 
 const Contacts = () => {
 
-  var subtitle;
-  const [modalIsOpen,setIsOpen] = React.useState(false);
-  function openModal() {
-      setIsOpen(true);
-  }
-  function afterOpenModal() {        
-  }
-  function closeModal(){
-      setIsOpen(false);
-  }
-
-
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
+
+  var subtitle;
+  const [modalIsOpen,setIsOpen] = React.useState(false);
+  function openModal() {
+
+  let ContactConfirm  = document.getElementsByClassName( 'c-contact-form__form' ),
+      alert_txt       = document.getElementsByClassName( 'alert-warning' ),
+      alert_txt_inner = document.getElementsByClassName( 'alert_inner_txt' ),
+      conf_name       = document.getElementsByClassName( 'confirm-name' ),
+      conf_mail       = document.getElementsByClassName( 'confirm-mail' ),
+      conf_ttl        = document.getElementsByClassName( 'confirm-title' ),
+      conf_body       = document.getElementsByClassName( 'confirm-body' );
+      
+      // エラーチェック
+      console.log(alert_txt);
+      if( alert_txt != undefined ) {
+        alert_txt[0].remove();
+      }
+      let alert_html = "<div ClassName='alert alert-warning'>"
+      if( name == "" ) {
+        alert_html += "<p ClassName='alert'>名前を入力してください。</p>";
+      }
+      if( email == "" ) {
+        alert_html += "<p ClassName='alert_inner_txt'>メールアドレスを入力してください</p>";
+      }
+      if( title == "" ) {
+        alert_html += "<p ClassName='alert_inner_txt'>タイトルを入力してください</p>";
+      }
+      if( body == "" ) {
+        alert_html += "<p ClassName='alert_inner_txt'>本文を入力してください</p>";
+      }
+      alert_html += "</div>";
+
+      ContactConfirm[0]
+        .insertAdjacentHTML(
+          'afterbegin', alert_html
+        );
+
+      // エラーがない場合はモーダルを表示する
+      if( alert_txt_inner.length == 0 ) {
+        setIsOpen(true);
+      }
+  }
+  function afterOpenModal() {        
+  }
+  function closeModal(){
+      setIsOpen(false);
+  }
   const handleSubmit = e => {
     e.preventDefault();
 
@@ -34,7 +70,6 @@ const Contacts = () => {
       title: title,
       body: body
     };
-
     axios({
       method: "post",
       url: "https://uemura5683.microcms.io/api/v1/contact",
@@ -44,17 +79,17 @@ const Contacts = () => {
         "X-WRITE-API-KEY": "e4f670a9-c5f8-4b37-b85c-420c00c33675"
       }
     })
-      .then(() => {
-        router.push("/success");
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    .then(() => {
+      // router.push("/success");
+    })
+    .catch(err => {
+      console.log(err);
+    });
   };
   return (
     <div className="c-contact-form__inner">
       <p className="c-contact-form__txt">どんな些細でもいいですので気軽にお問い合わせください！<br></br><a href="https://twitter.com/uemuragame5683" target="_blank">Twitter</a>でも受け付けております。</p>
-      <form>
+      <form className="c-contact-form__form">
         <div className="c-contact-form__content">
           <label>あなたの名前</label>
             <input
