ede9ef94d3cca5d5efa4c1ded6cc3a57014ca26f
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-11-03 04:43:00 +0900

    skillコンポーネントリファクタリング

--- components/framework/layout.tsx
+++ components/framework/layout.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
 import Logo from '../../components/framework/logo'
 import SnsLink from '../../components/framework/snslink'
--- components/top/skill.tsx
+++ components/top/skill.tsx
@@ -1,47 +1,16 @@
-import Mhtml  from '../extend/skill/html'
-import Mcss   from '../extend/skill/css'
-import Mjs    from '../extend/skill/js'
-import Mjq    from '../extend/skill/jq'
-import Mphp   from '../extend/skill/php'
-import Mscss  from '../extend/skill/scss'
-import Mpug   from '../extend/skill/pug'
-import Mgo    from '../extend/skill/go'
-import Mvue   from '../extend/skill/vue'
-import Mmt    from '../extend/skill/mt'
-import Mmcms  from '../extend/skill/mcms'
-import Mwp    from '../extend/skill/wp'
-import Mnuxt  from '../extend/skill/nuxt'
-import Mnext  from '../extend/skill/next'
-import Mgulp  from '../extend/skill/gulp'
-import Mbase  from '../extend/skill/base'
-import Mfigma from '../extend/skill/figma'
-import Mreact from '../extend/skill/react'
-import Mts    from '../extend/skill/ts'
-
+import Data from "../../json/data.json";
+import Skilllist from "../extend/skilllist";
 export default function Skill() {
   return (
     <section id="p-skill" className="u-gray has-animation" data-set-color="dark">
       <h2 className="c-title white">SKILL</h2>
       <ul className="skill__list">
-          <Mhtml></Mhtml>
-          <Mcss></Mcss>
-          <Mjs></Mjs>
-          <Mjq></Mjq>
-          <Mphp></Mphp>
-          <Mscss></Mscss>
-          <Mpug></Mpug>
-          <Mgo></Mgo>
-          <Mvue></Mvue>
-          <Mmt></Mmt>
-          <Mmcms></Mmcms>
-          <Mwp></Mwp>
-          <Mnuxt></Mnuxt>
-          <Mnext></Mnext>
-          <Mgulp></Mgulp>
-          <Mbase></Mbase>
-          <Mfigma></Mfigma>
-          <Mreact></Mreact>
-          <Mts></Mts>
+        {Data.content ? Data.content.map((Jsondata, index) => 
+            <Skilllist
+              data={Jsondata}
+              index={index}>
+            </Skilllist>
+          ): null}
       </ul>
     </section>
   )
